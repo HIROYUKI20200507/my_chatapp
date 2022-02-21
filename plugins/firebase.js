2d1704bd30e427a20f59603478a9c06ef07ae18e
Hiroyuki <dokomademo1124@gmail.com>
2022-02-21 10:01:36 +0900

    firebase yarn add

---
+++ plugins/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCPu8UlqSEUPo1oNVa-Qi44CZtLPe-TGBA',
+  authDomain: 'chatapp-b6126.firebaseapp.com',
+  projectId: 'chatapp-b6126',
+  storageBucket: 'chatapp-b6126.appspot.com',
+  messagingSenderId: '830923624059',
+  appId: '1:830923624059:web:5c2b6022adc96b8209dcde',
+}
+
+const app = initializeApp(firebaseConfig)
