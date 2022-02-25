09b3f1f5881b96d66d1a5e6aa4d576d9bda42e62
Hiroyuki <dokomademo1124@gmail.com>
2022-02-25 23:56:16 +0900

    ディレクトリ変更　CI確認

---
+++ src/pages/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <a class="btn btn-light" @click="login"> ログイン </a>
+  </div>
+</template>
+
+<script>
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    async login() {
+      const provider = new GoogleAuthProvider()
+      const auth = getAuth()
+
+      await signInWithPopup(auth, provider)
+      this.$router.push('/')
+    },
+  },
+}
+</script>
+
+<style></style>
