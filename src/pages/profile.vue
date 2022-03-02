86522f252da052f9b97448c555c31f80d8575bdb
Hiroyuki <dokomademo1124@gmail.com>
2022-03-02 09:25:43 +0900

    add tailwind css forms

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -1,46 +1,188 @@
 <template>
   <div class="mt-10 mx-52">
     <Header />
-    <section class="text-gray-600 body-font">
-      <div class="flex flex-col items-center justify-center px-5 py-24 mx-auto">
-        <img
-          class="object-cover object-center w-5/6 mb-10 rounded lg:w-2/6 md:w-3/6"
-          alt="hero"
-          :src="loginUser.img"
-        />
-        <div class="w-full text-center lg:w-2/3">
-          <h1
-            class="mb-4 text-3xl font-medium text-gray-900 title-font sm:text-4xl"
-          >
-            profile data
-          </h1>
-          <div class="flex items-center justify-center gap-12 my-8">
-            <div class="text-2xl font-medium text-gray-500 leadixlng-relaxed">
-              name
-            </div>
-            <input
-              type="text"
-              name=""
-              readonly
-              id="userName"
-              :value="loginUser.name"
-            />
-            <button
-              class="inline-flex px-3 py-1 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600"
-            >
-              編集
-            </button>
-          </div>
-          <div class="flex justify-center">
-            <button
-              class="inline-flex px-6 py-2 ml-4 text-lg text-gray-700 bg-gray-100 border-0 rounded focus:outline-none hover:bg-gray-200"
-            >
-              Button
-            </button>
+    <div>
+      <div class="md:grid md:grid-cols-3 md:gap-6">
+        <div class="md:col-span-1">
+          <div class="px-4 sm:px-0">
+            <h3 class="text-lg font-medium leading-6 text-gray-900">Profile</h3>
+            <p class="mt-1 text-sm text-gray-600">
+              This information will be displayed publicly so be careful what you
+              share.
+            </p>
           </div>
         </div>
+        <div class="mt-5 md:mt-0 md:col-span-2">
+          <form action="#" method="POST">
+            <div class="shadow sm:rounded-md sm:overflow-hidden">
+              <div class="px-4 py-5 space-y-6 bg-white sm:p-6">
+                <div class="grid grid-cols-6 gap-6">
+                  <div class="col-span-6 sm:col-span-3">
+                    <label
+                      for="first-name"
+                      class="block text-sm font-medium text-gray-700"
+                      >First name</label
+                    >
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autocomplete="given-name"
+                      class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-3">
+                    <label
+                      for="last-name"
+                      class="block text-sm font-medium text-gray-700"
+                      >Last name</label
+                    >
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autocomplete="family-name"
+                      class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+                <div class="grid grid-cols-3 gap-6">
+                  <div class="col-span-3 sm:col-span-2">
+                    <label
+                      for="company-website"
+                      class="block text-sm font-medium text-gray-700"
+                    >
+                      Website
+                    </label>
+                    <div class="flex mt-1 rounded-md shadow-sm">
+                      <span
+                        class="inline-flex items-center px-3 text-sm text-gray-500 border border-r-0 border-gray-300 rounded-l-md bg-gray-50"
+                      >
+                        http://
+                      </span>
+                      <input
+                        type="text"
+                        name="company-website"
+                        id="company-website"
+                        class="flex-1 block w-full border-gray-300 rounded-none focus:ring-indigo-500 focus:border-indigo-500 rounded-r-md sm:text-sm"
+                        placeholder="www.example.com"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    for="about"
+                    class="block text-sm font-medium text-gray-700"
+                  >
+                    About
+                  </label>
+                  <div class="mt-1">
+                    <textarea
+                      id="about"
+                      name="about"
+                      rows="3"
+                      class="block w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      placeholder="you@example.com"
+                    ></textarea>
+                  </div>
+                  <p class="mt-2 text-sm text-gray-500">
+                    Brief description for your profile. URLs are hyperlinked.
+                  </p>
+                </div>
+
+                <div>
+                  <label class="block text-sm font-medium text-gray-700">
+                    Photo
+                  </label>
+                  <div class="flex items-center mt-1">
+                    <span
+                      class="inline-block w-12 h-12 overflow-hidden bg-gray-100 rounded-full"
+                    >
+                      <svg
+                        class="w-full h-full text-gray-300"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"
+                        />
+                      </svg>
+                    </span>
+                    <button
+                      type="button"
+                      class="px-3 py-2 ml-5 text-sm font-medium leading-4 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      Change
+                    </button>
+                  </div>
+                </div>
+
+                <div>
+                  <label class="block text-sm font-medium text-gray-700">
+                    Cover photo
+                  </label>
+                  <div
+                    class="flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md"
+                  >
+                    <div class="space-y-1 text-center">
+                      <svg
+                        class="w-12 h-12 mx-auto text-gray-400"
+                        stroke="currentColor"
+                        fill="none"
+                        viewBox="0 0 48 48"
+                        aria-hidden="true"
+                      >
+                        <path
+                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+                      <div class="flex text-sm text-gray-600">
+                        <label
+                          for="file-upload"
+                          class="relative font-medium text-indigo-600 bg-white rounded-md cursor-pointer hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                        >
+                          <span>Upload a file</span>
+                          <input
+                            id="file-upload"
+                            name="file-upload"
+                            type="file"
+                            class="sr-only"
+                          />
+                        </label>
+                        <p class="pl-1">or drag and drop</p>
+                      </div>
+                      <p class="text-xs text-gray-500">
+                        PNG, JPG, GIF up to 10MB
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="px-4 py-3 text-right bg-gray-50 sm:px-6">
+                <button
+                  type="submit"
+                  class="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Save
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
-    </section>
+    </div>
+
+    <div class="hidden sm:block" aria-hidden="true">
+      <div class="py-5">
+        <div class="border-t border-gray-200"></div>
+      </div>
+    </div>
   </div>
 </template>
 
